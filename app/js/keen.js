5171ee7486626cc49dfb57ac1b976acc9cffa210
KMuncie <nivek2002@gmail.com>
2014-03-05 23:15:49 -0500

    Publish - Keen IO (commented out), Statcounter

--- app/js/keen.js
+++ app/js/keen.js
@@ -1,21 +1,21 @@
-   var Keen=Keen||{configure:function(e){this._cf=e},addEvent:function(e,t,n,i){this._eq=this._eq||[],this._eq.push([e,t,n,i])},setGlobalProperties:function(e){this._gp=e},onChartsReady:function(e){this._ocrq=this._ocrq||[],this._ocrq.push(e)}};(function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src=("https:"==document.location.protocol?"https://":"http://")+"dc8na2hxrj29i.cloudfront.net/code/keen-2.1.0-min.js";var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t)})();
+// var Keen=Keen||{configure:function(e){this._cf=e},addEvent:function(e,t,n,i){this._eq=this._eq||[],this._eq.push([e,t,n,i])},setGlobalProperties:function(e){this._gp=e},onChartsReady:function(e){this._ocrq=this._ocrq||[],this._ocrq.push(e)}};(function(){var e=document.createElement('script');e.type='text/javascript',e.async=!0,e.src=('https:'==document.location.protocol?'https://':'http://')+'dc8na2hxrj29i.cloudfront.net/code/keen-2.1.0-min.js';var t=document.getElementsByTagName('script')[0];t.parentNode.insertBefore(e,t)})();
 
-// Configure the Keen object with your Project ID and (optional) access keys.
-Keen.configure({
-   projectId: "5317e6d3ce5e431de3000002",
-   writeKey: "7508001912827e808c2b6f646735f0357dd63d3a88b60382107e8612f0cbb879e1c79648b791dec2e0bb0239cf58431768fb11059842f2013a7799a1b91aef4f84a4f4df6a267101b7b64f77229ffa7cefd36dac18fa2b8bf6693e49a2fb6f0a8e3a2725adf992cffdee1eb929aa5332", // required for sending events
-});
+// // Configure the Keen object with your Project ID and (optional) access keys.
+// Keen.configure({
+//    projectId: '5317e6d3ce5e431de3000002',
+//    writeKey: '7508001912827e808c2b6f646735f0357dd63d3a88b60382107e8612f0cbb879e1c79648b791dec2e0bb0239cf58431768fb11059842f2013a7799a1b91aef4f84a4f4df6a267101b7b64f77229ffa7cefd36dac18fa2b8bf6693e49a2fb6f0a8e3a2725adf992cffdee1eb929aa5332' // required for sending events
+// });
 
-var trackViewChanges = function () {
-   // create an event as a JS object
-   var ViewChange = {
-      viewContrast: "View Contrast Toggle",
-      viewExpand: "Toggle View Width"
-   };
+// var trackViewChanges = function () {
+//    // create an event as a JS object
+//    var ViewChange = {
+//       viewContrast: 'View Contrast Toggle',
+//       viewExpand: 'Toggle View Width'
+//    };
 
-   // add it to the "ViewChanges" collection
-   Keen.addEvent("ViewChanges", ViewChange);
-};
+//    // add it to the 'ViewChanges' collection
+//    Keen.addEvent('ViewChanges', ViewChange);
+// };
 
-// wherever the ViewChanges happens call the function
-trackViewChanges();
+// // wherever the ViewChanges happens call the function
+// trackViewChanges();
