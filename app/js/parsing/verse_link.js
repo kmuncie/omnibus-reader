0cb273ed8dfe7f44acf4a0a17f98156b7a29a33b
Ethan Smith <ethan@onebytegone.com>
2015-06-10 19:22:15 -0400

    Ref #7 Added verse link parser
    This doesn't add deep linking, but it does prevent the links from going to a 404 page.

---
+++ app/js/parsing/verse_link.js
@@ -0,0 +1,43 @@
+/**
+ * Verse link parsing functionality
+ */
+module.require(['debug', 'parser'], function(debug, BasicParser) {
+   'use strict';
+
+   var VerseLinkParser = BasicParser;
+   VerseLinkParser.prototype = {
+      parse: function(object) {
+         debug.log('Parsing verses in chapter: ' + object.citation);
+         object.html = this.convertLinks($("<div></div>").append(object.html)).html();
+
+         return object;
+      },
+
+      convertLinks: function(content) {
+         var self = this;
+         content.find('a').each(function() {
+            self.adjustLink($(this));
+         });
+
+         return content;
+      },
+
+      adjustLink: function(link) {
+         if (this.isVerseLink(link)) {
+            link.attr('href', link.attr('data-anchor'));
+         }
+      },
+
+      isVerseLink: function(link) {
+         var attr = link.attr('data-anchor');
+         var hasDataAnchor = (typeof attr !== typeof undefined && attr !== false);
+         var notFootnote = !link.hasClass("footnoteLink");
+
+         return hasDataAnchor && notFootnote
+      }
+   };
+
+   module.define('parser-verselink', (function() {
+      return new VerseLinkParser();
+   }()));
+});
