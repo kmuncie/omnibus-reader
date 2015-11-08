176b315eadbfb4be7fb66fab89a2dcab9be6435e
Ethan Smith <ethan@onebytegone.com>
2015-11-08 15:37:05 -0500

    Refs #17 Use cachable callbacks

--- app/js/fetcher.js
+++ app/js/fetcher.js
@@ -75,12 +75,79 @@ module.require(['debug'], function(debug) {
             return null;
          }
 
-         var promise;
+         /**
+          * ABOUT THE FOLLOWING REQUEST IMPLEMENTATION:
+          * In order to cache the ajax calls, we need to have a way to generate
+          * a callback param value that is unique to the call but won't change on
+          * repeated calls. Additionally instead of making multiple requests to the
+          * same endpoint, we can bundle these together.
+          *
+          * There are probably improvements to be made here. Even though this does
+          * work, please don't reuse without understanding what the implications
+          * of requesting like this are.
+          */
 
-         path = path + '?callback=?';
-         debug.log('Requesting ' + path);
+         var promise = $.Deferred(),
+             /**
+              * We need to use a consistant callback for each path in order to
+              * take advantage of caching. Might as well just use the app name
+              * with a key for the requested path.
+              */
+             callbackName = 'omnibusReader_' + path.replace(/\W+/g, ''),
+             previousCallback = window[callbackName];
 
-         promise = $.getJSON(path);
+         /**
+          * If we already have made a request for the url there will be a callback
+          * set for it. Since we are waiting for that request, let's just tag along
+          * for those results. No need to make more requests.
+          */
+         if (!previousCallback) {
+            debug.log('Requesting ' + path);
+            $.ajax({
+               url: path,
+               cache: true,
+               jsonpCallback: callbackName,
+               dataType: 'jsonp',
+            });
+         }
+
+         /**
+          * We have to create a callback on the window that will be
+          * called once the ajax response is received. This overwrites
+          * the last callback created for the given name. Therefore
+          * there is some interesting handling we have to perform so
+          * the old callbacks will still be called.
+          */
+         debug.log('Applying callback for ' + callbackName);
+         window[callbackName] = function(json, depth) {
+            // Depth is used for logging how many callbacks we have chained
+            if (typeof depth === 'undefined') {
+               depth = 0;
+            }
+
+            /**
+             * This is crucial to support the chaining of requests.
+             * Since we are overwriting the last callback and we want
+             * the last callback to be called, make sure to call it.
+             */
+            if (previousCallback) {
+               previousCallback(json, depth + 1);
+            }
+
+            debug.log('Resolve ' + callbackName + ' for depth: ' + depth);
+            promise.resolve(json);
+
+            /**
+             * Once we have ran the callback, go ahead and remove it. This
+             * way it doesn't get called again on the next request.
+             */
+            window[callbackName] = null;
+         };
+
+         /**
+          * Not needed for functionality, but it does give reassurance
+          * that the promise did run properly.
+          */
          promise.done(function() {
             debug.log('Response ' + path);
          });
